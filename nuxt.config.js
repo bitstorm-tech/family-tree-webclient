d38f4b92daab13e1cb6d1574ea5a7500aefa3fa5
Josef Bauer <bauerjoe@gmx.de>
2018-03-30 15:52:54 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'family-tree-webclient',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'A web application to manage family trees' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
